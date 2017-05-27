18815e5abf96ce2e0e93e6097542e6652b19a7eb
wojaya <wojaya@outlook.com>
2017-05-27 14:30:22 +0800

    Thread

--- app/src/main/java/com/example/jaya/bluetoothwear/MainActivity.java
+++ app/src/main/java/com/example/jaya/bluetoothwear/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.jaya.bluetoothwear;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,85 +8,35 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "IPMCWearATTENTION";
+    private static final String TAG = "IPMC>>>>>>>>>>";
     private static final boolean D = true;
     private static Intent intent = null;
 
-    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private static String address = "20:16:10:32:18:75";
+    Button btnOpenBt, btnCloseBt, btnConnBt;
 
-    private BluetoothAdapter btMyAdapter = null;
-    private BluetoothSocket btSocket = null;
-    private OutputStream myOutStream = null;
-
-    Button btnSendMsg;
-    TextView tvShowMsg;
+    private BluetoothAdapter btMyAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        intent = new Intent(MainActivity.this, ReadBluetoothService.class );
-
-        btnSendMsg = (Button) findViewById(R.id.btnSendMsg);
-
-        btnSendMsg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String message;
-                byte[] msgBuffer;
-                try {
-                    myOutStream = btSocket.getOutputStream();
-                } catch (IOException e) {
-                    Log.e(TAG, "ON CREATE: Output stream creation failed.", e);
-                }
-
-                message = "IPMC";
-                msgBuffer = message.getBytes();
-
-                try {
-                    myOutStream.write(msgBuffer);
-                } catch (IOException e) {
-                    Log.e(TAG, "ON CREATE: Exception during write.", e);
-                }
-
-            }
-        });
-
-        tvShowMsg = (TextView) findViewById(R.id.tvShowMsg);
+        findViewById();
 
         if (D) {
             Log.e(TAG, "+++ ON CREATE +++");
         }
 
-        btMyAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if (btMyAdapter == null) {
-            Toast.makeText(this, "蓝牙设备不可用，请打开蓝牙！", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-
-
         if (!btMyAdapter.isEnabled()) {
-            Toast.makeText(this,  "请打开蓝牙并重新运行程序！", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
-
-        if (D) {
-            Log.e(TAG, "+++ DONE IN ON CREATE, GOT LOCAL BT ADAPTER +++");
+            btnConnBt.setEnabled(false);
+            btnCloseBt.setEnabled(false);
+        } else {
+            btnOpenBt.setEnabled(false);
+            btnConnBt.setEnabled(true);
         }
     }
 
@@ -98,8 +46,9 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
 
         if (D) {
-            Log.e(TAG, "+++ ON START +++");
+            Log.e(TAG, "++++++ ON START ++++++");
         }
+
     }
 
     @Override
@@ -108,36 +57,8 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         if (D) {
-            Log.e(TAG, "+++ ON RESUME +++");
-            Log.e(TAG, "+++ ABOUT TO ATTEMPT CLIENT CONNECT +++");
+            Log.e(TAG, "++++++ ON RESUME ++++++");
         }
-
-        startService(intent);
-
-        DisplayToast("正在尝试连接设备，请稍后···");
-        BluetoothDevice device = btMyAdapter.getRemoteDevice(address);
-
-        try {
-            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-        } catch (IOException e) {
-            DisplayToast("套接字创建失败！");
-        }
-
-        DisplayToast("成功连接设备！");
-        btMyAdapter.cancelDiscovery();
-
-        try {
-            btSocket.connect();
-            DisplayToast("成功建立通道！");
-        } catch (IOException e) {
-            try {
-                btSocket.close();
-            } catch (IOException e2) {
-                DisplayToast("连接没有建立，无法关闭套接字！");
-            }
-        }
-
-        // Create a data stream so we can talk to server.
 
     }
 
@@ -147,22 +68,9 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         if (D) {
-            Log.e(TAG, "+++ ON PAUSE +++");
+            Log.e(TAG, "······· ON PAUSE ·······");
         }
 
-        if (myOutStream != null) {
-            try {
-                myOutStream.flush();
-            } catch (IOException e) {
-                Log.e(TAG, "ON PAUSE: Couldn't flush output stream.", e);
-            }
-        }
-
-        try {
-            btSocket.close();
-        } catch (IOException e2) {
-            DisplayToast("套接字关闭失败！");
-        }
     }
 
     @Override
@@ -171,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
 
         if (D) {
-            Log.e(TAG, "-- ON STOP --");
+            Log.e(TAG, "/////// ON STOP ///////");
         }
     }
 
@@ -181,17 +89,52 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         if (D) {
-            Log.e(TAG, "--- ON DESTROY ---");
+            Log.e(TAG, "====== ON DESTROY ======");
         }
 
         stopService(intent);
 
     }
 
-    public void DisplayToast(String str) {
+    private void findViewById () {
+        btnOpenBt = (Button) findViewById(R.id.btnOpenBt);
+        btnCloseBt = (Button) findViewById(R.id.btnCloseBt);
+        btnConnBt = (Button) findViewById(R.id.btnConnBt);
+    }
+
+    /* 开启蓝牙 */
+    public void onEnableButtonClicked (View view)
+    {
+        btMyAdapter.enable();
+
+        btnOpenBt.setEnabled(false);
+        btnConnBt.setEnabled(true);
+        btnCloseBt.setEnabled(true);
+    }
+
+
+    /* 关闭蓝牙 */
+    public void onDisableButtonClicked (View view)
+    {
+        btMyAdapter.disable();
+
+        btnConnBt.setEnabled(false);
+        btnOpenBt.setEnabled(true);
+        btnCloseBt.setEnabled(false);
+    }
+
+
+    /* 配对 */
+    public void onStartDiscoveryButtonClicked (View view)
+    {
+        intent = new Intent(MainActivity.this, ReadBluetoothService.class );
+        startService(intent);
+        btnConnBt.setEnabled(false);
+    }
+
+    public void DisplayToast (String str) {
         Toast toast=Toast.makeText(this, str, Toast.LENGTH_LONG);
         toast.setGravity(Gravity.TOP, 0, 220);
         toast.show();
     }
-
 }
--- app/src/main/java/com/example/jaya/bluetoothwear/ReadBluetoothService.java
+++ app/src/main/java/com/example/jaya/bluetoothwear/ReadBluetoothService.java
@@ -1,10 +1,39 @@
 package com.example.jaya.bluetoothwear;
 
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class ReadBluetoothService extends Service {
+
+    private static final String TAG = "IPMC<<<<<<<<<<";
+    private BluetoothAdapter myBluetoothAdapter = null;
+    public BluetoothSocket btSocket = null;
+    public OutputStream myOutStream = null;
+    public InputStream myInStream = null;
+
+//    In Method beginListenForData();
+    boolean stopWorkerTOF = false;
+    byte delimiter = 10;
+    int readBufferPosition = 0;
+    byte[] readBuffer = new byte[1024];
+    Handler handler = new Handler();
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static String address = "20:16:10:32:18:75";
+
     public ReadBluetoothService() {
     }
 
@@ -12,19 +41,27 @@ public class ReadBluetoothService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
-//        return null;
+//        return new MyBinder();
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        System.out.println("ReadBtService is Created");
+        Log.e("TAG", "ReadBluetoothService is Created");
+
+        CheckBt();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
-        System.out.println("ReadBtService is Started");
+        Log.e("TAG", "ReadBluetoothService is Started");
+
+        Connect();
+
+        writeData("IPMC");
+        DisplayToast("已发送字符串：IPMC");
+
         return START_STICKY;
     }
 
@@ -32,7 +69,138 @@ public class ReadBluetoothService extends Service {
     public void onDestroy()
     {
         super.onDestroy();
-        System.out.println("Service is Destroyed");
+        Log.e("TAG", "ReadBluetoothService is Destroyed");
+
+        try {
+            btSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to end the bluetoothConnection");
+        }
+    }
+
+
+//    public class MyBinder extends Binder {  }
+
+
+    private void CheckBt() {
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!myBluetoothAdapter.isEnabled()) {
+            DisplayToast("Bluetooth Disabled !");
+        }
+
+        if (myBluetoothAdapter == null) {
+            DisplayToast("Bluetooth null!");
+        }
+    }
+
+
+    public void Connect() {
+
+        BluetoothDevice device = myBluetoothAdapter.getRemoteDevice(address);
+
+        Log.e(TAG, "Connecting to ... " + device);
+
+        myBluetoothAdapter.cancelDiscovery();
+
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            btSocket.connect();
+            Log.e(TAG, "Connection made.");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                Log.e(TAG, "Unable to end the connection");
+            }
+            Log.e(TAG, "Socket creation failed");
+        }
+
+        beginListenForData();
+    }
+
+    private void writeData (String data) {
+        try {
+            myOutStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            Log.e(TAG, "Bug BEFORE Sending stuff", e);
+        }
+
+        String message = data;
+        byte[] msgBuffer = message.getBytes();
+
+        try {
+            myOutStream.write(msgBuffer);
+        } catch (IOException e) {
+            Log.e(TAG, "Bug while sending stuff", e);
+        }
+    }
+
+    public void beginListenForData() {
+        try {
+            myInStream = btSocket.getInputStream();
+        } catch (IOException e) {
+        }
+
+        Thread workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorkerTOF)
+                {
+                    try
+                    {
+                        int bytesAvailable = myInStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            myInStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+
+                                            DisplayToast("收到蓝牙信息：" + data);
+
+	                                        	/* You also can use Result.setText(data); it won't display multilines
+	                                        	*/
+
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorkerTOF = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+
+        Log.e("TAG", "InStream is started");
+    }
+
+    public void DisplayToast (String str) {
+        Toast toast=Toast.makeText(this, str, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.TOP, 0, 220);
+        toast.show();
     }
 
 }
